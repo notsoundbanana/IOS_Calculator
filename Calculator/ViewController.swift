b6c1ba7758358c4e4f0d94077dc07b22e8408f20
DANIL <soundbanana1@gmail.com>
2021-07-08 14:12:49 +0400

    Basic calculation
    !Bugs:
    - There is no way to continue counting with the result of previous calculations
    - .0 in not fractional numbers
    - Exceptions (dividing by 0)

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -10,39 +10,90 @@ import UIKit
 class ViewController: UIViewController {
 
     var numberFromScreen: Double = 0
+    var firstNum: Double = 0
+    var secondNum: Double = 0
+    var mathSign: Bool = false
+    var equalSign: Bool = false
+    var sign: Int = 0
     @IBOutlet weak var result: UILabel!
     
     //Работа с числами
     @IBAction func digits(_ sender: UIButton) {
-        result.text = result.text! + String(sender.tag)
+        if mathSign == true{
+            result.text = String(sender.tag)
+            mathSign = false
+        }
+        else if equalSign == true{
+            
+        }
+        else{
+            result.text = result.text! + String(sender.tag)
+        }
+        
         numberFromScreen = Double(result.text!)!
+        print(numberFromScreen)
     }
     
     // Работа с функциональными кнопками
     @IBAction func buttons(_ sender: UIButton) {
-            if result.text != ""{
-            if sender.tag == 10{ // AC
-                result.text = ""
-            }
-            
+        if result.text != "" && sender.tag != 15 && sender.tag != 10{
+            firstNum = Double(result.text!)!
             
             if sender.tag == 11{ // Деление
-                
+                result.text = "/";
+            }
+            else if sender.tag == 12{ // Умножение
+                result.text = "X";
+            }
+            else if sender.tag == 13{ // Вычитание
+                result.text = "-";
+            }
+            else if sender.tag == 14{ // Сложение
+                result.text = "+";
             }
             
-            if sender.tag == 12{ // Умножение
-                
+            sign = sender.tag
+            
+            print(sign, "sign")
+            mathSign = true
+            firstNum = numberFromScreen
+        }
+        
+        else if sender.tag == 10{
+            result.text = ""
+            firstNum = 0
+        }
+        else if sender.tag == 15{
+            equalSign = true
+            secondNum = numberFromScreen
+            
+            let mathSigns = [11: "/",
+                              12: "*",
+                              13: "-",
+                              14: "+"]
+            
+            if mathSigns[sign]! == "+"{
+                let res: Double = firstNum + secondNum
+                result.text = String(res)
             }
-            if sender.tag == 13{ // Вычитание
-                
+            else if mathSigns[sign]! == "-"{
+                let res: Double = firstNum - secondNum
+                result.text = String(res)
             }
-            if sender.tag == 14{ // Сложение
-                
+            else if mathSigns[sign]! == "/"{
+                let res: Double = firstNum / secondNum
+                result.text = String(res)
             }
+            else if mathSigns[sign]! == "*"{
+                let res: Double = firstNum * secondNum
+                result.text = String(res)
+            }
+            
         }
         
         
     }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
